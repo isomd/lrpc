b38c71fe3c2ef02323c08a8e3c5e8d7e9fd982eb
lixiaochun <3250162035@qq.com>
2025-08-04 17:26:41 +0800

    init

---
+++ src/test/java/com/xiaochun/lrpc/LrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaochun.lrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
